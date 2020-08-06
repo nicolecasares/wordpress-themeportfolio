9ad70fffd5d1d4883d76e6f12210e0dcddd0a1e3
Nicole Casares <nicolecasares@gmail.com>
2020-08-06 08:36:38 -0700

    first push

---
+++ nicole2020/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package nicole2020
+ */
+
+?>
+</div><!-- #page -->
+<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<?php dynamic_sidebar( 'footer-1' ); ?>
+			
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+
+<?php wp_footer(); ?>
+
+
+
+</body>
+</html>
